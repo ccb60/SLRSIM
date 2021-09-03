80d082be1827ee750a00931ce515c700b0f1605c
Curtis Bohlen <ccb60@users.noreply.github.com>
2021-09-02 22:44:21 -0400

    Add mock function skeletons for flood frequency simulation functions

---
+++ R/floodcast_arima.R
@@ -0,0 +1,16 @@
+
+
+
+#' Forecasting future coastal flood frequency based on time series simulation
+#'
+#' @param x
+#' @param y
+#' @param z
+#'
+#' @return
+#' @export
+#'
+#' @examples
+floodcast_arima <- function(x,y,z) {
+  print('In floodcast_arima')
+}
---
+++ R/floodcast_tub.R
@@ -0,0 +1,16 @@
+
+
+
+#' Forecasting future coastal flood frequency using a "Bathtub" model
+#'
+#' @param x
+#' @param y
+#' @param z
+#'
+#' @return
+#' @export
+#'
+#' @examples
+floodcast_tub <- function(x,y,z) {
+  print('In floodcast_tub')
+}
