d21141c63a069932289f2ebaef390242b7b6ac72
Curtis Bohlen <ccb60@users.noreply.github.com>
2021-09-17 20:57:05 -0400

    Add code, tests amd vignette content for build_deviations()

--- R/build_deviations.R
+++ R/build_deviations.R
@@ -3,7 +3,10 @@
 #'
 #' @inheritParams retrieve_data
 #'
-#' @return A dataframe with columns "datetime" and "water_level" and "deviation"
+#' @return A dataframe with columns "datetime", "water_level" and "deviation".
+#'         The values in "deviation" are the difference in observed and
+#'         predicted tides. Positive values mean observed water level was higher
+#'         than predicted.
 #' @export
 #'
 #' @examples
@@ -15,5 +18,45 @@ build_deviations <- function(.station, .first_yr, .last_yr,
                              .units = c('metric', 'english'),
                              .timefmt = c('gmt', 'lst', 'lst_ldt'),
                              .tz =  'UTC') {
-print('in build_deviations()')
+
+  .datum <- match.arg(.datum)
+  .units <- match.arg(.units)
+  .timefmt <- match.arg(.timefmt)
+
+  #If user does not specify the years, get what is available
+  if(missing(.first_yr) || missing(.last_yr)) {
+    timespan <- get_availability(.station)
+    if(missing(.first_yr)) .first_yr <- timespan$first_yr
+    if(missing(.last_yr)) .last_yr <- timespan$last_yr
+  }
+
+  # No parameter should have length > 1
+  stopifnot(length(.station) == 1)
+  stopifnot(length(.first_yr) == 1)
+  stopifnot(length(.last_yr) == 1)
+  message('Retrieving observed water levels....')
+  observed <- retrieve_data(.station  = .station,
+                            .first_yr = .first_yr,
+                            .last_yr  = .last_yr,
+                            .which    = 'observed',
+                            .datum    = .datum,
+                            .units    = .units,
+                            .timefmt  = .timefmt,
+                            .tz       =  .tz)
+  message('Retrieving predicted water levels....')
+  predicted <- retrieve_data(.station  = .station,
+                             .first_yr = .first_yr,
+                             .last_yr  = .last_yr,
+                             .which    = 'predicted',
+                             .datum    = .datum,
+                             .units    = .units,
+                             .timefmt  = .timefmt,
+                             .tz       =  .tz)
+  message('Calculating deviations....')
+  deviations <- dplyr::full_join(observed, predicted, by = 'datetime') %>%
+    dplyr::mutate(deviations = water_level - predicted ) %>%
+    dplyr::select(-predicted)
+  attr(deviations, 'which') <- NULL
+  message('Done!')
+  return(deviations)
 }
--- data-raw/make_test_data.R
+++ data-raw/make_test_data.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 ###############################
 #  utils
 ###############################
@@ -34,4 +36,26 @@ r <- httr::GET(BASE, query = parms)
 q <- httr::content(r)$data [1:5]
 p <- SLRSIM::.lst_2_df(q)
 saveRDS(p, 'tests/testthat/test_pred_df.RDS')
+
+#############################
+# build_deviations
+############################
+.begin_yr <- 2020
+.end_yr   <- 2020
+.station    <- '8454000'
+
+observed <- retrieve_data(.station,
+                          .begin_yr,
+                          .end_yr,
+                          .which = 'observed')
+predicted <- retrieve_data(.station,
+                           .begin_yr,
+                           .end_yr,
+                           .which = 'predicted')
+deviations <- dplyr::full_join(observed, predicted, by = 'datetime') %>%
+  mutate(deviations = water_level - predicted ) %>%
+  select(-predicted)
+deviations <- head(deviations, 10)
+attr(deviations, 'which') <- NULL
+saveRDS(deviations, 'tests/testthat/test_build_deviations.RDS')
 
